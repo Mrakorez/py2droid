a8964babd1a9d2130b85b2f7e35a95f9db94efdc
Mrakorez <deadline21th@gmail.com>
2025-12-12 17:11:57 +0200

    fix(module): ignore pip warnings about running as root

--- module/env.sh
+++ module/env.sh
@@ -5,6 +5,10 @@ export HOME="/data/adb/py2droid"
 export PATH="${HOME}/usr/bin:${HOME}/.local/bin:${PATH}"
 export LD_LIBRARY_PATH="${HOME}/usr/lib:${LD_LIBRARY_PATH}"
 
+# Ignore pip warnings about running as root
+# This is safe in our controlled environment
+export PIP_ROOT_USER_ACTION="ignore"
+
 export XDG_BIN_HOME="${HOME}/.local/bin"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_CONFIG_HOME="${HOME}/.config"
