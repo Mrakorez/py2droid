d0ef8af33efab564f4d80fae3851d44b2506e8d4
Mrakorez <deadline21th@gmail.com>
2026-01-09 19:52:46 +0200

    feat(module/customize)!: implement proper update workflow
    Implement a robust CPython update workflow consisting of 4 key steps:
    - Backs up the list of globally-installed pip packages from prefix installation.
    - Extracts the new CPython distribution into a temporary directory.
    - Purges all existing files from the prefix.
    - Moves the new CPython files into the prefix.
    
    Globally-installed packages are automatically backed up and can be restored
    after the update.
    
    BREAKING CHANGE: The prefix will be completely purged during update.

--- module/customize.sh
+++ module/customize.sh
@@ -20,23 +20,81 @@ is_installed() {
   " 2>/dev/null
 }
 
-extract_cpython() {
+get_stdlib_path() {
+  python3 -c "
+    from sysconfig import get_path
+    print(get_path('stdlib'))
+  " 2>/dev/null
+}
+
+backup_packages() {
+  local needs_backup=false
+  local packages_file="${EXTERNAL_STORAGE:-/sdcard}/py2droid-packages.txt"
+  local site_packages
+
+  site_packages="$(get_stdlib_path)/site-packages"
+
+  # Check for globally-installed packages in the prefix installation
+  for file in "${site_packages}"/*; do
+    [[ ! -e ${file} ]] && continue
+    case "${file##*/}" in
+      *.pth | pip | pip-*.dist-info | README.txt) continue ;;
+      *)
+        ui_print "- Found globally-installed packages"
+        ui_print "  Will back up to: ${packages_file}"
+        needs_backup=true
+        break
+        ;;
+    esac
+  done
+  ${needs_backup} || return 0
+
+  if ! which pip3 >/dev/null; then
+    abort "! pip not found, cannot back up packages"
+  fi
+
+  if ! pip3 freeze --path "${site_packages}" >"${packages_file}"; then
+    abort "! Failed to back up packages"
+  fi
+}
+
+install_cpython() {
+  local already_installed
   local tarball="cpython-${ARCH}.tar.xz"
+  local temp
 
   if ! unzip -l "${ZIPFILE}" | grep -q "${tarball}"; then
     abort "! CPython for ${ARCH} not found"
   fi
 
-  mkdir -p "${PYTHONHOME}"
+  if is_installed; then
+    already_installed=true
+    backup_packages
+  else
+    already_installed=false
+  fi
+
+  temp=$(mktemp -d -p "${TMPDIR}") || abort "! Failed to create temporary directory"
 
   ui_print "- Extracting CPython (${tarball})..."
-  if ! unzip -p "${ZIPFILE}" "${tarball}" | tar -xJC "${PYTHONHOME}" --strip-components 1; then
+  if ! unzip -p "${ZIPFILE}" "${tarball}" | tar -xJC "${temp}" --strip-components 1; then
+    abort "! Failed to extract CPython tarball"
+  fi
+
+  if ${already_installed}; then
+    ui_print "- Removing old installation..."
+    rm -rf "${PYTHONHOME}"
+  fi
+  mkdir -p "${PYTHONHOME}"
+
+  ui_print "- Installing CPython..."
+  if ! find "${temp}" -mindepth 1 -maxdepth 1 -exec mv {} "${PYTHONHOME}/" +; then
     # Cleanup on failure for fresh installs
-    if ! is_installed; then
+    if ! ${already_installed}; then
       rm -rf "${PYTHONHOME}"
       rmdir --ignore-fail-on-non-empty "${HOME}"
     fi
-    abort "! Failed to extract CPython tarball"
+    abort "! Failed to install CPython"
   fi
 }
 
@@ -61,7 +119,7 @@ main() {
 
   . ./env.sh || abort "! Failed to source env.sh"
 
-  extract_cpython
+  install_cpython
   create_env_dirs
   set_permissions
   finalize_install
